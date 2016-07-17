285b3cce19b3d4420334dce3d445f74d45ba5d51
Ricardo Pereira <m@ricardopereira.eu>
2016-07-17 14:56:41 +0100

    Init

---
+++ Screensketch/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Screensketch
+//
+//  Created by Ricardo Pereira on 17/07/16.
+//  Copyright © 2016 Ricardo Pereira. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = DrawViewController()
+        return true
+    }
+
+}
---
+++ Screensketch/DrawViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DrawViewController.swift
+//  Screensketch
+//
+//  Created by Ricardo Pereira on 17/07/16.
+//  Copyright © 2016 Ricardo Pereira. All rights reserved.
+//
+
+import UIKit
+
+class DrawViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .redColor()
+    }
+
+}
