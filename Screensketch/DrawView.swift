c301417351f47f6184eafc2dc3f88650be967ad6
Ricardo Pereira <m@ricardopereira.eu>
2016-07-17 15:19:56 +0100

    Add comments

--- Screensketch/DrawView.swift
+++ Screensketch/DrawView.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class DrawView: UIView {
 
+    var drawColor: UIColor = UIColor.blackColor()
+    var drawWidth: CGFloat = 10.0
+
+    private var path: UIBezierPath = UIBezierPath()
+
     override init(frame: CGRect) {
         super.init(frame: frame)
         internalInit()
@@ -29,8 +34,8 @@ class DrawView: UIView {
     // MARK: Drawing a path
 
     override func drawRect(rect: CGRect) {
-        // 4. Redraw whole rect, ignoring parameter. Please note we always invalidate whole view.
-        let context = UIGraphicsGetCurrentContext()
+        // Redraw whole rect, ignoring parameter
+        // (Please note we always invalidate whole view)
         self.drawColor.setStroke()
         self.path.lineWidth = self.drawWidth
         self.path.lineCapStyle = CGLineCap.Round
@@ -41,7 +46,6 @@ class DrawView: UIView {
     // MARK: Gestures
 
     private func setupGestureRecognizers() {
-        // 1. Set up a pan gesture recognizer to track where user moves finger
         let panRecognizer = UIPanGestureRecognizer(target: self, action: #selector(DrawView.handlePan))
         self.addGestureRecognizer(panRecognizer)
     }
@@ -53,12 +57,10 @@ class DrawView: UIView {
             self.startAtPoint(point)
         case .Changed:
             self.continueAtPoint(point)
-        case .Ended:
-            self.endAtPoint(point)
-        case .Failed:
+        case .Ended, .Failed:
             self.endAtPoint(point)
         default:
-            assert(false, "State not handled")
+            break
         }
     }
 
@@ -70,20 +72,14 @@ class DrawView: UIView {
     }
 
     private func continueAtPoint(point: CGPoint) {
-        // 2. Accumulate points as they are reported by the gesture recognizer, in a bezier path object
+        // Accumulate points as they are reported by the gesture recognizer, in a bezier path object
         self.path.addLineToPoint(point)
-
-        // 3. Trigger a redraw every time a point is added (finger moves)
+        // Trigger a redraw every time a point is added (finger moves)
         self.setNeedsDisplay()
     }
 
     private func endAtPoint(point: CGPoint) {
         // Nothing to do when ending/cancelling for now
     }
-
-    var drawColor: UIColor = UIColor.blackColor()
-    var drawWidth: CGFloat = 10.0
-
-    private var path: UIBezierPath = UIBezierPath()
 
 }
